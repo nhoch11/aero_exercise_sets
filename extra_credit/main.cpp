cc6fb1b9555194e17cea499bf9ce7d4b2c9c3c40
NATHAN HOCH LAPTOP <nathan.h.hoch@gmail.com>
2023-11-05 23:35:41 -0700

    m

---
+++ extra_credit/main.cpp
@@ -0,0 +1,21 @@
+//#include "hoch_helper.h"
+#include "aircraft.h"
+
+int main (int argc, char * const argv[])
+{
+    cout<<"Program started: \n"<<endl;
+    
+
+
+
+    aircraft* my_aircraft = new aircraft("vertical_trim.json");
+    
+    my_aircraft->init_sim();
+    printf("Running sim\n\n");
+    my_aircraft->run_sim();
+
+    
+    cout<<"Program completed: "<<endl;
+
+    return 0;
+}
