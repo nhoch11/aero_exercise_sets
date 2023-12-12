e621412805be5b3c3f51ab433f5551d9508d891a
Nathan Hoch <nathan.hoch@usu.edu>
2023-12-12 10:06:28 -0700

    hfd

--- Exercise Set 7/7.9/main.cpp
+++ Exercise Set 7/7.9/main.cpp
@@ -5,17 +5,23 @@ int main (int argc, char * const argv[])
 {
     cout<<"Program started: \n"<<endl;
 
-<<<<<<< HEAD
-    aircraft* my_aircraft = new aircraft("aircraft_7_7_3.json");
-=======
-    aircraft* my_aircraft = new aircraft("aircraft_6_6_test.json");
->>>>>>> c375a94b9fab333801684dcbabc26d7154b2fd94
+    //aircraft* my_aircraft = new aircraft("aircraft_6_6_test.json");
     
-    my_aircraft->init_sim();
-    printf("Running sim\n\n");
-    my_aircraft->run_sim();
+    //my_aircraft->init_sim();
+    //printf("Running sim\n\n");
+    //my_aircraft->run_sim();
+    double A[4], B[4], ans[4];
+    A[0] = 1.0;
+    A[1] = 2.0;
+    A[2] = 3.0;
+    A[3] = 4.0;
+    B[0] = 1.0;
+    B[1] = -2.0;
+    B[2] = -3.0;
+    B[3] = -4.0;
+    quat_mult(A, B, ans);
+    array_print(ans, 4);
 
-    
     cout<<"Program completed: "<<endl;
 
     return 0;
